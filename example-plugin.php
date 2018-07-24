1cb030b100d617c4094ab4c8d392f24865c62f69
Crews <dcrews@IST-APP-ML-0030.ad.bu.edu>
2018-07-24 08:55:02 -0400

    Install linting for project. Create main plugin file.

---
+++ example-plugin.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Plugin's bootstrap file to launch the plugin.
+ *
+ * @package     Example-Plugin
+ * @author      Dan Crews (@DannyCrews)
+ * @license     GPL2+
+ *
+ * @wordpress-plugin
+ * Plugin Name: Example Plugin
+ * Plugin URI:  https://www.bu.edu
+ * Description: An plugin containing example blocks.
+ * Version:     0.1.0
+ * Author:      Dan Crews (@DannyCrews)
+ * Author URI:  https://twitter.com/dannycrews
+ * Text Domain: exampleplugin
+ * Domain Path: /languages
+ * License:     GPL2+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+
