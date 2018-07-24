a027b9d2f1ee6b6ba214b93ce77e7b22a2efa208
Crews <dcrews@IST-APP-ML-0030.ad.bu.edu>
2018-07-24 10:01:56 -0400

    Add paths for js and css assets.

--- example-plugin.php
+++ example-plugin.php
@@ -17,6 +17,44 @@
  * Domain Path: /languages
  * License:     GPL2+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ **/
+
+namespace DannyCrews\Example_Plugin;
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Gets this plugin's absolute directory path.
+ *
+ * @since  2.1.0
+ * @ignore
+ * @access private
+ *
+ * @return string
  */
+function _get_plugin_directory() {
+	return __DIR__;
+}
 
+/**
+ * Gets this plugin's URL.
+ *
+ * @since  2.1.0
+ * @ignore
+ * @access private
+ *
+ * @return string
+ */
+function _get_plugin_url() {
+	static $plugin_url;
 
+	if ( empty( $plugin_url ) ) {
+		$plugin_url = plugins_url( null, __FILE__ );
+	}
+
+	return $plugin_url;
+}
+
+// Enqueue JS and CSS
+include __DIR__ . '/lib/enqueue-scripts.php';
